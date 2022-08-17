30ad7b60409db5f5b6ccd21bf457cb884f3ff5c9
gbertl <gilbertlcsndle@gmail.com>
2022-08-17 16:15:00 +0800

    apply inline form for mobile in personal-info

--- pages/js/personal-info.js
+++ pages/js/personal-info.js
@@ -1,12 +1,15 @@
 document
-  .querySelectorAll('[data-application=true]')[1]
-  .querySelector('#personal-info-fields-identity-row-action')
-  .addEventListener('click', () => {
-    document.querySelector('#goverment-id-modal').classList.remove('d-none');
+  .querySelectorAll('#personal-info-fields-identity-row-action')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+      document.body.classList.add('overflow-y-hidden');
+      document.querySelector('#goverment-id-modal').classList.remove('d-none');
+    });
   });
 
-document
-  .querySelector('#goverment-id-modal__close')
-  .addEventListener('click', () => {
+document.querySelectorAll('#goverment-id-modal__close').forEach((element) => {
+  element.addEventListener('click', () => {
+    document.body.classList.remove('overflow-y-hidden');
     document.querySelector('#goverment-id-modal').classList.add('d-none');
   });
+});
