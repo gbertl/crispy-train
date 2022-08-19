31162379eebe95b9eb933d7a5794794d37bd27db
gbertl <gilbertlcsndle@gmail.com>
2022-08-19 18:00:22 +0800

    add where to, when, who

--- guestPages/js/homepage.js
+++ guestPages/js/homepage.js
@@ -3,3 +3,50 @@ handleModal({
   modalSelector: '#filter-modal',
   btnCloseSelector: '._oda838',
 });
+
+document.querySelector('#anywhere-btn').addEventListener('click', () => {
+  document.querySelector('.cxy853f').classList.replace('cxy853f', 'cn7o9ly');
+  document.querySelector('.lr5v90m').classList.replace('lr5v90m', 'l1h6eamc');
+  document.querySelector('.b1t24mo5').classList.replace('b1t24mo5', 'b12rw3s8');
+  document
+    .querySelector('#search-tabpanel')
+    .classList.replace('c1oqx6sn', 'ceyij5b');
+  document.querySelector('.ix85u26').classList.replace('ix85u26', 'i1eek2sl');
+  document.querySelector('.bu69x9v').classList.replace('bu69x9v', 'b14gupvm');
+  document.querySelector('.c8th90').classList.add('d-none');
+  document.querySelector('#search-btn-text').classList.remove('d-none');
+  document.querySelector('#search-backdrop').classList.remove('d-none');
+  document.querySelector('.p1jg7hjv').classList.add('p5mmcz9');
+});
+
+document.querySelector('#anyweek-btn').addEventListener('click', () => {
+  document.querySelector('.cxy853f').classList.replace('cxy853f', 'cn7o9ly');
+  document.querySelector('.lr5v90m').classList.replace('lr5v90m', 'l1h6eamc');
+  document.querySelector('.b1t24mo5').classList.replace('b1t24mo5', 'b12rw3s8');
+  document.querySelector('.b174x59c').classList.replace('b174x59c', 'b1odri3w');
+  document.querySelector('#search-backdrop').classList.remove('d-none');
+  document.querySelector('#anyweek-panel').classList.remove('d-none');
+  document
+    .querySelector('#search-tabpanel')
+    .classList.replace('c1oqx6sn', 'ceyij5b');
+  document.querySelector('#search-btn-text').classList.remove('d-none');
+  document.querySelector('.c8th90').classList.add('d-none');
+  document.querySelector('.bu69x9v').classList.replace('bu69x9v', 'b14gupvm');
+});
+
+document.querySelector('#addguests-btn').addEventListener('click', () => {
+  document.querySelector('.cxy853f').classList.replace('cxy853f', 'cn7o9ly');
+  document.querySelector('.lr5v90m').classList.replace('lr5v90m', 'l1h6eamc');
+  document.querySelector('.b1t24mo5').classList.replace('b1t24mo5', 'b12rw3s8');
+  document
+    .querySelector('#search-tabpanel')
+    .classList.replace('c1oqx6sn', 'ceyij5b');
+  document.querySelector('#search-backdrop').classList.remove('d-none');
+  document.querySelector('#search-btn-text').classList.remove('d-none');
+  document.querySelector('.c8th90').classList.add('d-none');
+  document.querySelector('.bu69x9v').classList.replace('bu69x9v', 'b14gupvm');
+  document
+    .querySelector('[data-testid=structured-search-input-field-guests-button]')
+    .classList.replace('b174x59c', 'b1odri3w');
+  document.querySelector('#search-who').classList.remove('d-none');
+});
