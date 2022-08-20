10d5543f724f557783777db715bd763e391bb8e9
gbertl <gilbertlcsndle@gmail.com>
2022-08-20 11:12:11 +0800

    add signup form, logout state signup_login

--- pages/js/signup_login.js
+++ pages/js/signup_login.js
@@ -17,3 +17,47 @@ document.querySelectorAll('.continue-btn-phone').forEach((el) => {
     parent.querySelector('.continue-btn-phone').classList.add('d-none');
   });
 });
+
+document.querySelectorAll('form').forEach((form) => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formType = e.target.classList[0];
+
+    const parent = e.target.closest('[data-application=true]');
+
+    const loginSignup = parent.querySelector('[data-login-signup]');
+    const formList = {
+      'phone-form': parent.querySelector('[data-confirm-number]'),
+      'email-form': parent.querySelector('[data-finish-signup]'),
+    };
+
+    const targetForm = formList[formType];
+
+    loginSignup.classList.add('d-none');
+
+    targetForm.classList.remove('d-none');
+
+    targetForm
+      .querySelector('[data-testid="panel-header-button"]')
+      .addEventListener('click', (e) => {
+        targetForm.classList.add('d-none');
+        loginSignup.classList.remove('d-none');
+      });
+  });
+});
+
+(function getPage() {
+  const params = new URLSearchParams(window.location.search);
+
+  if (params.has('has_user_session')) {
+    return document.querySelectorAll('[data-application]').forEach((el) => {
+      if (!el.hasAttribute('data-user-session')) {
+        return el.remove();
+      }
+      el.classList.remove('d-none');
+    });
+  }
+
+  document.querySelector('[data-user-session]').remove();
+})();
