6d21f689595f327b1defca26f8d0f302ea9334ca
gbertl <gilbertlcsndle@gmail.com>
2022-09-05 21:48:43 +0800

    add chat-bubble

---
+++ assets/js/pages/products.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', () => {
+  if (document.documentElement.scrollTop) {
+    document
+      .querySelector('._1v7a7680')
+      .classList.replace('_1v7a7680', '_128pof1u');
+  } else {
+    document
+      .querySelector('._128pof1u')
+      .classList.replace('_128pof1u', '_1v7a7680');
+  }
+});
