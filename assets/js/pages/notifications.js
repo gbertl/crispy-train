d75e317d3d62a6eeea36a32d66d85b12d2cb0b6c
IanDestura <aiyuuu033119@gmail.com>
2022-09-06 01:06:06 +0800

    rename page.css 4

--- assets/js/pages/notifications.js
+++ assets/js/pages/notifications.js
@@ -2,8 +2,8 @@ document.querySelectorAll('button[role=tab]').forEach((el) => {
   el.addEventListener('click', function () {
     document
       .querySelectorAll('button[role=tab]')
-      .forEach((el) => el.classList.replace('_10br0yfc', '_6tvt8jj'));
-    this.classList.replace('_6tvt8jj', '_10br0yfc');
+      .forEach((el) => el.classList.replace('inbox-container__tab-notification', 'inbox-container__tab-btn'));
+    this.classList.replace('inbox-container__tab-btn', 'inbox-container__tab-notification');
 
     document
       .querySelectorAll('div[role=tabpanel]')
@@ -15,7 +15,7 @@ document.querySelectorAll('button[role=tab]').forEach((el) => {
 });
 
 document.querySelector('#filter-menu').addEventListener('click', function () {
-  this.classList.toggle('_1arxx82');
-  this.classList.toggle('_1ym0gspv');
+  this.classList.toggle('inbox-container__inbox-btn-filter');
+  this.classList.toggle('inbox-container__filter-menu-btn');
   document.querySelector('#filter-menu-dropdown').classList.toggle('hidden');
 });
