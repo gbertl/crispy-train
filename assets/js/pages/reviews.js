84255ca558f5814f0d8fbecc33e5989928848cff
gbertl <gilbertlcsndle@gmail.com>
2022-09-12 16:43:27 +0800

    rename & refactor progress-bar

--- assets/js/pages/reviews.js
+++ assets/js/pages/reviews.js
@@ -12,13 +12,13 @@ document.querySelector('.btn-dark').addEventListener('click', (e) => {
 
   // update scale in progressbar
   if (counter === 0) {
-    document.querySelector('._1rcz3vn').style.transform = 'scaleX(0.333333)';
+    document.querySelector('.progress-bar-bottom__bar').style.transform = 'scaleX(0.333333)';
   } else if (counter === 1) {
-    document.querySelector('._1rcz3vn').style.transform = 'scaleX(0.5)';
+    document.querySelector('.progress-bar-bottom__bar').style.transform = 'scaleX(0.5)';
   } else if (counter === 2) {
-    document.querySelector('._1rcz3vn').style.transform = 'scaleX(0.666667)';
+    document.querySelector('.progress-bar-bottom__bar').style.transform = 'scaleX(0.666667)';
   } else if (counter === 3) {
-    document.querySelector('._1rcz3vn').style.transform = 'scaleX(0.833333)';
+    document.querySelector('.progress-bar-bottom__bar').style.transform = 'scaleX(0.833333)';
   }
 
   // show back button
@@ -43,13 +43,13 @@ document.querySelector('._13ea8wp').addEventListener('click', (e) => {
 
   // update scale in progressbar
   if (counter === 1) {
-    document.querySelector('._1rcz3vn').style.transform = 'scaleX(0.166667)';
+    document.querySelector('.progress-bar-bottom__bar').style.transform = 'scaleX(0.166667)';
   } else if (counter === 2) {
-    document.querySelector('._1rcz3vn').style.transform = 'scaleX(0.333333)';
+    document.querySelector('.progress-bar-bottom__bar').style.transform = 'scaleX(0.333333)';
   } else if (counter === 3) {
-    document.querySelector('._1rcz3vn').style.transform = 'scaleX(0.5)';
+    document.querySelector('.progress-bar-bottom__bar').style.transform = 'scaleX(0.5)';
   } else if (counter === 4) {
-    document.querySelector('._1rcz3vn').style.transform = 'scaleX(0.666667)';
+    document.querySelector('.progress-bar-bottom__bar').style.transform = 'scaleX(0.666667)';
   }
 
   if (counter === 4) {
