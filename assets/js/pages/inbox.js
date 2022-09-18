653dd5fcf386c2dd30ea6287f8f9cfe997b3b102
IanDestura <aiyuuu033119@gmail.com>
2022-09-18 14:51:45 +0800

    Add filter experiences (click on 1st modal to trigger)

--- assets/js/pages/inbox.js
+++ assets/js/pages/inbox.js
@@ -1,13 +1,43 @@
-// open modal btn
-document
-  .querySelector('#host-inbox-filter-button')
-  .addEventListener('click', () => {
-    document.querySelector('#show-filter-modal').classList.remove('hidden');
-  });
+// // open modal btn
+// document
+//   .querySelector("#host-inbox-filter-button")
+//   .addEventListener("click", () => {
+//     document.querySelector("#show-filter-modal").classList.remove("hidden");
+//   });
 
-// close modal btn
-document
-  .querySelector('#show-filter-modal__close')
-  .addEventListener('click', () => {
-    document.querySelector('#show-filter-modal').classList.add('hidden');
+// // close modal btn
+// document
+//   .querySelector("#show-filter-modal__close")
+//   .addEventListener("click", () => {
+//     document.querySelector("#show-filter-modal").classList.add("hidden");
+//   });
+
+
+function openModal(trigger, modal, callback) {
+  document
+  .querySelector(trigger).addEventListener("click", () => {
+    document.querySelector(modal).classList.remove("hidden");
+    if(callback){
+      callback()
+    }
   });
+}
+
+function closeModal(trigger, modal, callback) {
+  document
+  .querySelector(trigger).addEventListener("click", () => {
+    document.querySelector(modal).classList.add("hidden");
+    if(callback){
+      callback()
+    }
+  });
+}
+
+openModal("#host-inbox-filter-button", "#show-filter-modal");
+closeModal("#show-filter-modal__close", "#show-filter-modal");
+openModal("#filter-experiences-button", "#show-filter-experience-modal", () => {
+  document.querySelector("#show-filter-modal").classList.add("hidden");
+});
+closeModal("#show-filter-experience-modal__close", "#show-filter-experience-modal", () => {
+  document.querySelector("#show-filter-modal").classList.remove("hidden");
+});
