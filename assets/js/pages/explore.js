22a052ce540b4ba65adf92646f892e4b3e56c0b3
gbertl <gilbertlcsndle@gmail.com>
2022-10-11 22:13:24 +0800

    implement heart button to all

--- assets/js/pages/explore.js
+++ assets/js/pages/explore.js
@@ -441,26 +441,6 @@ discoverSunnyCard.addEventListener('mouseleave', () => {
   });
 });
 
-const btnUnsave = document.querySelector('.js-btn-unsave');
-
-btnUnsave.addEventListener('click', (e) => {
-  e.currentTarget.classList.add('hidden');
-  e.currentTarget
-    .closest('.card-alt-4')
-    .querySelector('.js-btn-save')
-    .classList.remove('hidden');
-});
-
-const wishlistModalBtns = document.querySelectorAll('._11eqlma4');
-const btnSave = document.querySelector('.js-btn-save');
-
-wishlistModalBtns.forEach((el) => {
-  el.addEventListener('click', (e) => {
-    btnUnsave.classList.remove('hidden');
-    btnSave.classList.add('hidden');
-    e.currentTarget.closest('#save-modal').classList.add('hidden');
-  });
-});
 
 document.querySelectorAll('.card-alt-1__media-container').forEach((el) => {
   el.addEventListener('mouseenter', (e) => {
