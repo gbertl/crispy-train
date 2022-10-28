486f9e6385cdcd27601585f9fec6f28568521500
IanDestura <idestura@hris-system.com>
2022-10-29 00:25:00 +0800

    landing page no. 2 A - fx

--- assets/js/pages/accessibility.js
+++ assets/js/pages/accessibility.js
@@ -12,3 +12,50 @@ window.addEventListener('scroll', (e) => {
     }
     // document.getElementById("scroll_position").textContent=scroll;
 })
+
+
+if(document.querySelector('._virdaj') != null){
+    const close = document.querySelector('._virdaj');
+    const modal = document.querySelector('.modal-filter-list');
+    close.addEventListener('click', (e) => {
+        if(e.target.classList.contains('_virdaj')){
+            modal.classList.replace('_wheg71v', '_dsadsad')
+            setTimeout(() => {
+                document.querySelector('._virdaj').classList.add('hidden') 
+            }, 500);
+            document.querySelector('._1t0jy5tw').classList.add('hidden')
+        }
+    });
+}
+
+if(document.querySelector('#close-modal-filter-list') != null){
+    const close = document.querySelector('#close-modal-filter-list');
+    const modal = document.querySelector('.modal-filter-list');
+    close.addEventListener('click', () => {
+        modal.classList.replace('_wheg71v', '_dsadsad')
+        setTimeout(() => {
+            document.querySelector('._virdaj').classList.add('hidden') 
+        }, 500);
+        document.querySelector('._1t0jy5tw').classList.add('hidden')
+    });
+}
+
+if(document.querySelector('#open-modal-filter-list-m') != null){
+    const close = document.querySelector('#open-modal-filter-list-m');
+    const modal = document.querySelector('.modal-filter-list');
+    close.addEventListener('click', () => {
+        modal.classList.replace('_dsadsad', '_wheg71v')
+        document.querySelector('._virdaj').classList.remove('hidden') 
+        document.querySelector('._1t0jy5tw').classList.remove('hidden')
+    });
+}
+
+if(document.querySelector('#open-modal-filter-list') != null){
+    const close = document.querySelector('#open-modal-filter-list');
+    const modal = document.querySelector('.modal-filter-list');
+    close.addEventListener('click', () => {
+        modal.classList.replace('_dsadsad', '_wheg71v')
+        document.querySelector('._virdaj').classList.remove('hidden') 
+        document.querySelector('._1t0jy5tw').classList.remove('hidden')
+    });
+}
